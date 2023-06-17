b84e86a520cee67162d06d02a63bb2e239fc1319
Lewismer <youssef.merrah@usmba.ac.ma>
2023-06-17 01:32:32 -0700

    commiting task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,17 +19,17 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n>0)
 	{
-		printf(" the number %d is positive\n",n);
+		printf("%d is positive\n",n);
 	}
 	else
 	{
 		if(n==0)
 		{
-			printf("the number %d is zero\n",n);
+			printf("%d is zero\n",n);
 		}
 		else
 		{
-			printf("the number %d is negative\n",n);
+			printf("%d is negative\n",n);
 	    	}
 	}
 		
