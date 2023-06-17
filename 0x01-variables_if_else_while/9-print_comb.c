d5d475e4028e6fab063210439880ca9e7d629868
Lewismer <youssef.merrah@usmba.ac.ma>
2023-06-17 13:07:29 -0700

    adding task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of this program
+ *
+ * Description: this function serves to print from 0 to 10
+ *		using only putchar twice
+ *
+ * Return: 0 on successful execution
+*/
+int main(void)
+{
+	int n = '0';
+	int m = 0;
+
+	while (m++ < 10)
+	{
+		putchar(n++);
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
