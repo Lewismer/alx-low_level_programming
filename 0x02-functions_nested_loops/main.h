d7a5cc857d6c30f96ee7542bc27f693dca7080dc
Lewismer <youssef.merrah@usmba.ac.ma>
2023-06-21 15:45:29 -0700

    task0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * _putchar - print a character 
+ *
+ * @c : the caracter to be printed
+ *
+ * Return: 1 
+ */
+
 int _putchar(char c);
 
-
-
-
-
 #endif
-
-
