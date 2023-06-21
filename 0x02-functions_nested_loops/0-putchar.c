c3e45e673e4375f4d6baadb1365fca4e18bf789c
Lewismer <youssef.merrah@usmba.ac.ma>
2023-06-21 04:37:48 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main : Entry point of this program
+ *
+ * Description:		This function serves to print _putchar
+ *
+ * Return: 0 on successful execution
+ */
+int main(void)
+{
+	_putchar("_putchar\0");
+
+	return (0);
+}
