850335160e3fa1fcf110950edd703ef298ee413b
Lewismer <youssef.merrah@usmba.ac.ma>
2023-06-23 13:55:58 +0000

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
- /**
-  * main : check the code 
-  *
-  * Description:	This function serves to print _putchar
-  *
-  * Return: 0 on successful execution
+/**
+ * main - check the code 
+ *
+ * Description:	This function serves to print _putchar
+ *
+ * Return: 0 on successful execution
  */
 int main(void)
 {
