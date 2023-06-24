5d59827cc56bb48620d7b5292935c81b6197f561
Lewismer <youssef.merrah@usmba.ac.ma>
2023-06-24 10:33:12 +0000

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - print alphabets in lower case 10 times
+ * print_alphabet_x10 - print alphabets in lower case 10 times
  */
 void print_alphabet_x10(void)
 {
