b37bf9b38741e6a79a49769072960d502bb33c2c
Lewismer <youssef.merrah@usmba.ac.ma>
2023-06-24 12:36:36 +0000

    task9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * times_table - print the times table from 0 to 9
+ */
+void times_table(void)
+{
+	int l, c, res;
+
+	for (l = 0; l < 10; l++)
+	{
+		_putchar('0');
+		for (c = 0; c < 10; c++)
+		{
+			res = l * c;
+			if (c <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (res > 9)
+				_putchar((res / 10 + '0');
+			else 
+				_putchar('0');
+			_putchar((res % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
