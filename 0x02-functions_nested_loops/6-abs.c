5469fedc07826b93e93f71161809fb3ca416d731
Lewismer <youssef.merrah@usmba.ac.ma>
2023-06-24 11:40:30 +0000

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - return the absolute value of a number.
+ * @n: The number of which the absolue value is calculated.
+ *
+ * Return: the absolute value of the number
+ */
+int _abs(int n)
+{
+
+	if (n > 0)
+
+		return (n);
+	else
+		return (-n);
+
+}
