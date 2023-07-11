87d43d648425a25eeff7f265565527d48daba7a4
Lewismer <youssef.merrah@usmba.ac.ma>
2023-07-11 05:18:16 -0700

    adding task3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - the entry point of this program
+ * @argc: the number of arguments
+ * @argv: the vector of arguments
+ * Description:		this program prints all the arguments of main
+ *
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc > 1)
+	{
+		result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", result);
+	}
+	else
+	{
+		printf("Error\n");
+		return (-1);
+	}
+
+	return (0);
+}
